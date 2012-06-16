25deec2352c688ab243ad38316991eb4acf0ffe1
Petr Miko <miko.petr@gmail.com>
2012-06-16 15:57:01 +0200

    Added Time pickers into AddRecordActivity

--- src/cz/zcu/kiv/eeg/lab/reservation/AddRecordActivity.java
+++ src/cz/zcu/kiv/eeg/lab/reservation/AddRecordActivity.java
@@ -5,14 +5,22 @@ import java.util.Calendar;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class AddRecordActivity extends Activity {
+public class AddRecordActivity extends Activity implements OnClickListener {
 
 	private static final String TAG = AddRecordActivity.class.getSimpleName();
+
+	private int year, month, day, fromHour, fromMinute, toHour, toMinute;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,21 @@ public class AddRecordActivity extends Activity {
 		ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
 
+		Bundle b = getIntent().getExtras();
+		year = b.getInt("year");
+		month = b.getInt("month") + 1;
+		day = b.getInt("day");
+
 		initFields();
+		initButtons();
+	}
+
+	private void initButtons() {
+		Button changeFrom = (Button) findViewById(R.id.fromTimeButton);
+		Button changeTo = (Button) findViewById(R.id.toTimeButton);
+
+		changeFrom.setOnClickListener(this);
+		changeTo.setOnClickListener(this);
 	}
 
 	private void initFields() {
@@ -30,14 +52,9 @@ public class AddRecordActivity extends Activity {
 		TextView fromField = (TextView) findViewById(R.id.fromField);
 		TextView toField = (TextView) findViewById(R.id.toField);
 
-		Bundle b = getIntent().getExtras();
-		int year = b.getInt("year");
-		int month = b.getInt("month");
-		int day = b.getInt("day");
-
 		Calendar c = Calendar.getInstance();
 		SimpleDateFormat sf = new SimpleDateFormat("HH:mm");
-		dateField.setText(String.format("%d.%d.%d", day, month + 1, year));
+		dateField.setText(String.format("%d.%d.%d", day, month, year));
 
 		fromField.setText(sf.format(c.getTime()));
 		toField.setText(sf.format(c.getTime()));
@@ -52,5 +69,48 @@ public class AddRecordActivity extends Activity {
 			break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	private OnTimeSetListener fromListener = new OnTimeSetListener() {
+
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			fromHour = hourOfDay;
+			fromMinute = minute;
+
+			TextView fromField = (TextView) findViewById(R.id.fromField);
+			fromField.setText(String.format("%02d:%02d", fromHour, fromMinute));
+
+		}
+	};
+
+	private OnTimeSetListener toListener = new OnTimeSetListener() {
+
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			toHour = hourOfDay;
+			toMinute = minute;
+
+			TextView toField = (TextView) findViewById(R.id.toField);
+			toField.setText(String.format("%02d:%02d", toHour, toMinute));
+
+		}
+	};
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.fromTimeButton:
+			TimePickerDialog fromDialog = new TimePickerDialog(this,
+					fromListener, fromHour, fromMinute, true);
+			fromDialog.show();
+			break;
+		case R.id.toTimeButton:
+			TimePickerDialog toDialog = new TimePickerDialog(this, toListener,
+					toHour, toMinute, true);
+			toDialog.show();
+			break;
+		}
+
 	}
 }
